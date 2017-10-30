4bc9f1cb20b902a583c2337dde85cceb61c49d47
Ross <ross.tienken99@gmail.com>
2017-10-30 13:50:45 -0600

    working on stats bug

--- js/team.js
+++ js/team.js
@@ -22,74 +22,63 @@ $(document).ready(function () {
       "Authorization": "Basic " + btoa('RossTienken' + ":" + 'q1Project')
     },
   }
-  var skateStats = {
-    skatePts: {
-      "async": true,
-      "url": `https://api.mysportsfeeds.com/v1.1/pull/nhl/2017-2018-regular/cumulative_player_stats.json?team=${$src}&playerstats=G,A,Pts,Pim&sort=stats.Pts.D`,
-      "method": "GET",
-      headers: {
-        "Authorization": "Basic " + btoa('RossTienken' + ":" + 'q1Project')
-      },
+  var skatePts = {
+    "async": true,
+    "url": `https://api.mysportsfeeds.com/v1.1/pull/nhl/2017-2018-regular/cumulative_player_stats.json?team=${$src}&playerstats=Pts&sort=stats.Pts.D`,
+    "method": "GET",
+    headers: {
+      "Authorization": "Basic " + btoa('RossTienken' + ":" + 'q1Project')
     },
-    skateGoal: {
-      "async": true,
-      "url": `https://api.mysportsfeeds.com/v1.1/pull/nhl/2017-2018-regular/cumulative_player_stats.json?team=${$src}&playerstats=G,A,Pts,Pim&sort=stats.G.D`,
-      "method": "GET",
-      headers: {
-        "Authorization": "Basic " + btoa('RossTienken' + ":" + 'q1Project')
-      },
-    },
-    skateAssist: {
-      "async": true,
-      "url": `https://api.mysportsfeeds.com/v1.1/pull/nhl/2017-2018-regular/cumulative_player_stats.json?team=${$src}&playerstats=G,A,Pts,Pim&sort=stats.A.D`,
-      "method": "GET",
-      headers: {
-        "Authorization": "Basic " + btoa('RossTienken' + ":" + 'q1Project')
-      },
-    },
-    skatePim: {
-      "async": true,
-      "url": `https://api.mysportsfeeds.com/v1.1/pull/nhl/2017-2018-regular/cumulative_player_stats.json?team=${$src}&playerstats=G,A,Pts,Pim&sort=stats.Pim.D`,
-      "method": "GET",
-      headers: {
-        "Authorization": "Basic " + btoa('RossTienken' + ":" + 'q1Project')
-      },
-    }
   }
-  var goalStats = {
-    goalWin: {
-      "async": true,
-      "url": `https://api.mysportsfeeds.com/v1.1/pull/nhl/2017-2018-regular/cumulative_player_stats.json?team=${$src}&playerstats=W,GAA,Sv%25,SO&sort=stats.W.D`,
-      "method": "GET",
-      headers: {
-        "Authorization": "Basic " + btoa('RossTienken' + ":" + 'q1Project')
-      },
+  var skateGoal = {
+    "async": true,
+    "url": `https://api.mysportsfeeds.com/v1.1/pull/nhl/2017-2018-regular/cumulative_player_stats.json?team=${$src}&playerstats=G&sort=stats.G.D`,
+    "method": "GET",
+    headers: {
+      "Authorization": "Basic " + btoa('RossTienken' + ":" + 'q1Project')
     },
-    goalGAA: {
-      "async": true,
-      "url": `https://api.mysportsfeeds.com/v1.1/pull/nhl/2017-2018-regular/cumulative_player_stats.json?team=${$src}&playerstats=W,GAA,Sv%25,SO&sort=stats.GAA.D`,
-      "method": "GET",
-      headers: {
-        "Authorization": "Basic " + btoa('RossTienken' + ":" + 'q1Project')
-      },
-    },
-    goalSv: {
-      "async": true,
-      "url": `https://api.mysportsfeeds.com/v1.1/pull/nhl/2017-2018-regular/cumulative_player_stats.json?team=${$src}&playerstats=W,GAA,Sv%25,SO&sort=stats.Sv%25.D`,
-      "method": "GET",
-      headers: {
-        "Authorization": "Basic " + btoa('RossTienken' + ":" + 'q1Project')
-      },
-    },
-    goalShut: {
-      "async": true,
-      "url": `https://api.mysportsfeeds.com/v1.1/pull/nhl/2017-2018-regular/cumulative_player_stats.json?team=${$src}&playerstats=W,GAA,Sv%25,SO&sort=stats.SO.D`,
-      "method": "GET",
-      headers: {
-        "Authorization": "Basic " + btoa('RossTienken' + ":" + 'q1Project')
-      },
-    }
   }
+  var skateAssist = {
+    "async": true,
+    "url": `https://api.mysportsfeeds.com/v1.1/pull/nhl/2017-2018-regular/cumulative_player_stats.json?team=${$src}&playerstats=A&sort=stats.A.D`,
+    "method": "GET",
+    headers: {
+      "Authorization": "Basic " + btoa('RossTienken' + ":" + 'q1Project')
+    },
+  }
+  var skatePim = {
+    "async": true,
+    "url": `https://api.mysportsfeeds.com/v1.1/pull/nhl/2017-2018-regular/cumulative_player_stats.json?team=${$src}&playerstats=Pim&sort=stats.Pim.D`,
+    "method": "GET",
+    headers: {
+      "Authorization": "Basic " + btoa('RossTienken' + ":" + 'q1Project')
+    },
+  }
+  var goalWin = {
+    "async": true,
+    "url": `https://api.mysportsfeeds.com/v1.1/pull/nhl/2017-2018-regular/cumulative_player_stats.json?team=${$src}&playerstats=W&sort=stats.W.D`,
+    "method": "GET",
+    headers: {
+      "Authorization": "Basic " + btoa('RossTienken' + ":" + 'q1Project')
+    },
+  }
+  var goalGAA = {
+    "async": true,
+    "url": `https://api.mysportsfeeds.com/v1.1/pull/nhl/2017-2018-regular/cumulative_player_stats.json?team=${$src}&playerstats=GAA&sort=stats.GAA.D`,
+    "method": "GET",
+    headers: {
+      "Authorization": "Basic " + btoa('RossTienken' + ":" + 'q1Project')
+    },
+  }
+  var goalSv = {
+    "async": true,
+    "url": `https://api.mysportsfeeds.com/v1.1/pull/nhl/2017-2018-regular/cumulative_player_stats.json?team=${$src}&playerstats=Sv%25&sort=stats.Sv%25.D`,
+    "method": "GET",
+    headers: {
+      "Authorization": "Basic " + btoa('RossTienken' + ":" + 'q1Project')
+    },
+  }
+
   $.ajax(games).done(function (response) {
     let newGames = response.teamgamelogs.gamelogs;
     for(let i = newGames.length -1; i > 0; i--){
@@ -125,11 +114,55 @@ $(document).ready(function () {
     }
   });
 
-  $.ajax(skateStats).done(function (response) {
-    console.log(response)
+  $.ajax(skatePts).done(function (response) {
+    let player = response.cumulativeplayerstats.playerstatsentry["0"];
+    let $points = $('#points');
+
+    $points.append(`<h4>${player.player.FirstName} ${player.player.LastName}</h4> <h5>${player.stats.stats.Points['#text']}</h5>`)
+
   });
 
-  $.ajax(goalStats).done(function (response) {
-    console.log(response)
+  $.ajax(skateGoal).done(function (response) {
+    let $goals = $('#goals');
+    let player = response.cumulativeplayerstats.playerstatsentry["0"];
+
+    $goals.append(`<h4>${player.player.FirstName} ${player.player.LastName}</h4><h5>${player.stats.stats.Goals['#text']}</h5>`)
   });
+
+  $.ajax(skateAssist).done(function (response) {
+    let $assists = $('#assists');
+    let player = response.cumulativeplayerstats.playerstatsentry["0"];
+
+    $assists.append(`<h4>${player.player.FirstName} ${player.player.LastName}</h4><h5>${player.stats.stats.Assists['#text']}</h5>`)
+  });
+
+  $.ajax(skatePim).done(function (response) {
+    let $pim = $('#pim');
+    let player = response.cumulativeplayerstats.playerstatsentry["0"];
+
+    $pim.append(`<h4>${player.player.FirstName} ${player.player.LastName}</h4><h5>${player.stats.stats.PenaltyMinutes['#text']}</h5>`);
+  });
+
+
+  $.ajax(goalWin).done(function (response) {
+    let $wins = $('#wins');
+    let player = response.cumulativeplayerstats.playerstatsentry["0"];
+
+    $wins.append(`<h4>${player.player.FirstName} ${player.player.LastName}</h4><h5>${player.stats.stats.Wins['#text']}</h5>`)
+  });
+
+  $.ajax(goalGAA).done(function (response) {
+    let $gAA = $('#gAA');
+    let player = response.cumulativeplayerstats.playerstatsentry["0"];
+
+    $gAA.append(`<h4>${player.player.FirstName} ${player.player.LastName}</h4><h5>${player.stats.stats.GoalsAgainstAverage['#text']}</h5>`)
+  });
+
+  $.ajax(goalSv).done(function (response) {
+    let $saveP = $('#saveP');
+    let player = response.cumulativeplayerstats.playerstatsentry["0"];
+
+    $saveP.append(`<h4>${player.player.FirstName} ${player.player.LastName}</h4><h5>${player.stats.stats.SavePercentage['#text']}%</h5>`)
+  });
+
 })
